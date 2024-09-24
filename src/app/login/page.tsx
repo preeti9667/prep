397b64173dfe3978bbf1955cd5bbb83e6c08b757
preeti9667 <saini04preeti@gmail.com>
2024-09-24 10:20:46 +0530

    add new branch

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
+
 const Login = () => (
     <>
-    <h1>Login page !</h1>
+    <h1  className=''>Login page !</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit sequi debitis deleniti suscipit laboriosam quasi, magni neque rerum vel doloremque nam explicabo tempora ullam eos quis illum alias. Aliquid, magni.</p>
     </>
 );
 
