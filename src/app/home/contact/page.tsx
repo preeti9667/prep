4295650f18f1fdf4b4422562d9dada6ac26fd90d
preeti9667 <saini04preeti@gmail.com>
2024-10-08 11:40:33 +0530

    delete p

--- src/app/home/contact/page.tsx
+++ src/app/home/contact/page.tsx
@@ -1,8 +1,7 @@
 const Contact = () => (
     <>
     <h1>Contact page !</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quis aperiam, debitis laboriosam laudantium delectus nihil omnis, id ut molestias odit expedita nostrum, dolorem earum. Officiis officia ullam ratione non!</p>
-    <div>hello i am content </div>
+   
     </>
 );
 
